c7107098fb7140103d3342bacb0cd035e38e69fc
chumi0796 <114894664+chumi0796@users.noreply.github.com>
2022-10-15 12:31:43 -0500

    Create conexion.php
    conexion DB

---
+++ bd/conexion.php
@@ -0,0 +1,11 @@
+<?php
+$host ="localhost";
+$usuario= "root";
+$clave="123";
+$bd="roleyDB";
+$conexion=mysqli_connect($host,$usuario,$clave,$bd);
+/*if($conexion){
+	echo "conectado correctamente";
+}else{
+	echo "no se pudo conectar";}*/
+?>
