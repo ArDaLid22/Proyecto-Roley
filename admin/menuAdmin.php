98513bb58a2842cb1a41c771a5fef1c883f88dbd
roa0796 <114894664+chumi0796@users.noreply.github.com>
2022-10-17 17:23:31 -0500

    F5

--- admin/menuAdmin.php
+++ admin/menuAdmin.php
@@ -5,9 +5,12 @@
     <meta charset="utf-8">
     <title>RolayStore - Admin</title>
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
     <!-- FontAwesome Icons -->
     <script src="https://kit.fontawesome.com/8ef4f0069f.js" crossorigin="anonymous"></script>
 </head>
@@ -48,7 +51,7 @@
                     <option value="3">Agotado</option>
                 </select>
             </div>
-            <input type="submit"  value="Registrar" ></button>
+            <input type="submit" value="Registrar"></button>
         </form>
 
         <div class="col-8 p-4">
@@ -65,8 +68,6 @@
                         <th>estadoID</th>
                         <th></th>
                         <th></th>
-
-
                     </tr>
                     </tr>
                 </thead>
@@ -74,20 +75,20 @@
 
                     <?php
                     include "../bd/conexion.php";
-                    $sql = $conexion->query("select * from producto");
+                    $sql = $conexion->query("SELECT producto.productoID, producto.nombre AS prodName, categoria.nombre AS catName, producto.descripcion, producto.imagen, producto.stock, producto.precio, estado.nombre AS estName FROM producto INNER JOIN categoria ON producto.categoriaID = categoria.categoriaID INNER JOIN estado ON producto.estadoID = estado.estadoID;");
                     while ($datos = $sql->fetch_object()) { ?>
 
-                        <tr>
-                            <th><?= $datos->nombre ?></th>
-                            <th><?= $datos->categoriaID ?></th>
-                            <th><?= $datos->descripcion ?></th>
-                            <th><img src="data:image/jpg;base64, <?php echo base64_encode($datos->imagen); ?>" style="max-height: 100px"></th>
-                            <th><?= $datos->stock ?></th>
-                            <th><?= $datos->precio ?></th>
-                            <th><?= $datos->estadoID ?></th>
-                            <th><a href="actualizar.php?id=<?php echo $d['productoID'] ?>" class="btn btn-info">Editar</a></th>
-                            <th><a href="delete.php?id=<?php echo $d['productoID'] ?>" class="btn btn-danger">Eliminar</a></th>
-                        </tr>
+                    <tr>
+                        <th><?= $datos->prodName ?></th>
+                        <th><?= $datos->catName ?></th>
+                        <th><?= $datos->descripcion ?></th>
+                        <th><img src="data:image/jpg;base64, <?php echo base64_encode($datos->imagen); ?>" style="max-height: 100px"></th>
+                        <th><?= $datos->stock ?></th>
+                        <th><?= $datos->precio ?></th>
+                        <th><?= $datos->estName ?></th>
+                        <th><a href="actualizar.php?id=<?php echo $d['productoID'] ?>" class="btn btn-info">Editar</a></th>
+                        <th><a href="delete.php?id=<?php echo $d['productoID'] ?>" class="btn btn-danger">Eliminar</a></th>
+                    </tr>
                     <?php }
 
                     ?>
@@ -95,9 +96,6 @@
             </table>
 
         </div>
-
-
-
     </div>
 
 </html>
