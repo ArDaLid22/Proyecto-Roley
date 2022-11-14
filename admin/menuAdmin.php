1dd95af8dbb9966bc32fa7fd8bfbc603ef8a54bd
roa0796 <114894664+chumi0796@users.noreply.github.com>
2022-11-13 21:27:10 -0500

    actualizacion interface

--- admin/menuAdmin.php
+++ admin/menuAdmin.php
@@ -55,49 +55,9 @@
         </form>
 
         <div class="col-8 p-4">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>CategoriaID</th>
-                        <th>Descripcion</th>
-                        <th>Imagen</th>
-                        <th>Stock</th>
-                        <th>precio</th>
-                        <th>estadoID</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    <?php
-                    require "../bd/conexion.php";
-                    $sql = "SELECT producto.productoID, producto.nombre AS prodName, categoria.nombre AS catName, producto.descripcion, producto.imagen, producto.stock, producto.precio, estado.nombre AS estName FROM producto INNER JOIN categoria ON producto.categoriaID = categoria.categoriaID INNER JOIN estado ON producto.estadoID = estado.estadoID ORDER BY producto.productoID DESC";
-                    $result = $conexion->query($sql);
-                    while ($row = $result->fetch_assoc()) {
-                    ?>
-
-                    <tr>
-                        <th><?= $row['prodName'] ?></th>
-                        <th><?= $row['catName'] ?></th>
-                        <th><?= $row['descripcion'] ?></th>
-                        <th><img src="data:image/jpg;base64, <?php echo base64_encode($row['imagen']); ?>" style="max-height: 100px"></th>
-                        <th><?= $row['stock'] ?></th>
-                        <th><?= $row['precio'] ?></th>
-                        <th><?= $row['estName'] ?></th>
-                        <th><a href="actualizar.php?id=<?php echo $row['productoID'] ?>" class="btn btn-info">Editar</a></th>
-                        <th><a href="../bd/delete.php?id=<?php echo $row['productoID'] ?>" class="btn btn-danger">Eliminar</a></th>
-                    </tr>
-
-                    <?php }
-
-                    ?>
-                </tbody>
-            </table>
-
+            <?php
+            include "../bd/read.php";
+            ?>
         </div>
     </div>
 
